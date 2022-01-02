36df568e882feaaba9c89ffcab14f6659c0357a2
isor <ratkovszki.istvn@gmail.com>
2022-01-02 18:28:37 +0100

    Adds navigation component

--- wedding/src/app/app-routing.module.ts
+++ wedding/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InviteComponent } from './component/invite/invite.component';
 import { ProgramComponent } from './component/program/program.component';
 import { ResponseComponent } from './component/response/response.component';
 
 const routes: Routes = [
   {path: 'response', component: ResponseComponent},
+  {path: 'invite', component: InviteComponent},
   {path: 'program', component: ProgramComponent},
   {path: '', redirectTo:'program', pathMatch:'full'}
 ];
