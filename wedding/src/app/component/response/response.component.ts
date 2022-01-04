5b847bcf897f2df5cfcc9691b6a2325bfdcd4265
isor <ratkovszki.istvn@gmail.com>
2022-01-04 19:07:22 +0100

    Updates response screen

--- wedding/src/app/component/response/response.component.ts
+++ wedding/src/app/component/response/response.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Invitee } from 'src/app/model/invitee';
+import { GsheetService } from 'src/app/service/gsheet.service';
 
 @Component({
   selector: 'app-response',
@@ -7,9 +10,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResponseComponent implements OnInit {
 
-  constructor() { }
+  private hash: string = "";
 
-  ngOnInit(): void {
+  private invitees: Invitee[] = [];
+  invitee: Invitee | undefined;
+  
+  constructor(private activeRoute: ActivatedRoute, private gsheetService: GsheetService) { 
+    this.hash = activeRoute.snapshot.paramMap.get("hash")!!;
   }
 
+  ngOnInit(): void {
+    this.gsheetService.getInvitees().subscribe(data => {
+      this.processInvitees(data);
+    });
+  }
+
+  processInvitees(data: any) {
+    data.values.forEach((element: any) => {
+      var invitee = new Invitee();
+      invitee.hash = element[0];
+      invitee.validity = element[1];
+      invitee.name = element[2];
+      invitee.type = element[3];
+      this.invitees.push(invitee);
+    });
+    this.getContent();
+  }
+
+  getContent() {
+    var invitee = this.invitees.find(invitee => invitee.hash == this.hash);
+    if(invitee instanceof Invitee) {
+      this.invitee = invitee!!;
+    } else {
+      this.invitee = this.invitees.find(invitee => invitee.hash == 'error');
+    }
+  }
 }
