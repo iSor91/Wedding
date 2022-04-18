e499849c9a4faab14cf871541c2da61e46f5274e
isör <ratkovszki.istvn@gmail.com>
2022-04-18 22:09:42 +0200

    Adds generation information

--- seating_cards/seating_cards.py
+++ seating_cards/seating_cards.py
@@ -2,6 +2,8 @@ import os
 import shutil
 from card_generator import CardGenerator
 from google_sheet_service import GoogleSheetService
+from datetime import datetime
+
 
 dir_path = 'out'
 
@@ -16,12 +18,19 @@ cg = CardGenerator()
 
 gs = GoogleSheetService()
 
+start_time = datetime.now()
+print('Started seating card generation at {}'.format(start_time) )
+all = 1
 cnt = 0
 for r in gs.all_response_names:
     subcnt = 0
     for name in r:
-        print('generating seating card for {}'.format(name))
+        print('{} - generating seating card for {}'.format(all, name))
         img = cg.generate_image(name)
         img.save('out/' + str(cnt) + '_' + str(subcnt) + '.png')
         subcnt = subcnt + 1
+        all = all + 1
     cnt = cnt + 1
+
+end_time = datetime.now()
+print('Image generation ended at {} after {}'.format(end_time, end_time-start_time))
