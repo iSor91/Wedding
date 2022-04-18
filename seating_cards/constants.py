2265b15d22feba1c2c7948ba528150229cff9750
isör <ratkovszki.istvn@gmail.com>
2022-04-18 17:29:51 +0200

    Updated text processing to be able to handle any length of sentences.

--- seating_cards/constants.py
+++ seating_cards/constants.py
@@ -3,3 +3,38 @@ image_w = 1920 #full image width
 image_h = 1080 #full image height
 head_d  = 450  #head circle diameter
 padding = 50   #padding on the image
+
+row_offset = 1.25
+
+umlaut_chars = ['ä', 'ö', 'ü', 'Ä', 'Ö', 'Ü']
+long_umlaut_chars = ['ő', 'ű', 'Ő', 'Ű']
+
+tittle_chars = ['i']
+long_tittle_chars = ['á', 'é', 'í', 'ó', 'ú', 'Á', 'É', 'Í', 'Ó', 'Ú']
+
+character_counterparts = {
+    'ä': 'a',
+    'á': 'a',
+    'é': 'e',
+    'i': 'i',
+    'í': 'i',
+    'ó': 'o',
+    'ö': 'o',
+    'ő': 'o',
+    'ú': 'u',
+    'ü': 'u',
+    'ű': 'u',
+
+    'Ä': 'A',
+    'Á': 'A',
+    'É': 'E',
+    'I': 'I',
+    'Í': 'I',
+    'Ó': 'O',
+    'Ö': 'O',
+    'Ő': 'O',
+    'Ú': 'U',
+    'Ü': 'U',
+    'Ű': 'U'
+
+}
--- seating_cards/pil_drawer.py
+++ seating_cards/pil_drawer.py
@@ -1,5 +1,6 @@
-from PIL import ImageDraw
-
+from email.policy import default
+import constants as c
+import math
 
 class PilDrawer:
 
@@ -14,23 +15,33 @@ class PilDrawer:
         self.d.rectangle([(x,y),(x+w,y+h)], fill)
 
     #text
-    def print_char(self,char, start, fnt):
-        self.d.text((start[0]+2,start[1]+2), char, font = fnt, fill = (0,0,0))
-        self.d.text(start, char, font = fnt, fill = (255,255,255))
+    def print_char(self,char, start, fnt, size):
+        char_to_print = c.character_counterparts.get(char, char)
+        self.d.text((start[0]+2,start[1]+2), char_to_print, font = fnt, fill = (0,0,0))
+        self.d.text(start, char_to_print, font = fnt, fill = (255,255,255))
 
-    def print_tittle(self,char, start, fnt):
-        print_accent(char,start,fnt,'.', 0, size / 4)
+        if(char in c.long_tittle_chars):
+            self.print_long_tittle(char_to_print, start, fnt, size)
+        if(char in c.tittle_chars):
+            self.print_tittle(char_to_print, start, fnt, size)
+        if(char in c.umlaut_chars):
+            self.print_umlaut(char_to_print, start, fnt, size)
+        if(char in c.long_umlaut_chars):
+            self.print_long_umlaut(char_to_print, start, fnt, size)
 
-    def print_umlaut(self,char, start, fnt):
-        print_accent(char,start,fnt,'..', 0, size / 4)
+    def print_tittle(self, char, start, fnt, size):
+        self.print_accent(char,start,fnt,'.', 0, size / 4, size)
 
-    def print_long_tittle(self,char,start,fnt):
-        print_accent(char,start,fnt,',', size/10, size / 8)
+    def print_umlaut(self, char, start, fnt, size):
+        self.print_accent(char,start,fnt,'..', 0, size / 4, size)
 
-    def print_long_umlaut(self,char,start,fnt):
-        print_accent(char,start,fnt,',,', size/10, size / 8)
+    def print_long_tittle(self, char,start,fnt, size):
+        self.print_accent(char,start,fnt,'\'', size/10, size / 1.5, size)
 
-    def print_accent(self,char, start, fnt, accent, x_diff, y_diff):
+    def print_long_umlaut(self, char,start,fnt, size):
+        self.print_accent(char,start,fnt,'\"', size/10, size / 1.5, size)
+
+    def print_accent(self,char, start, fnt, accent, x_diff, y_diff, size):
         tittle_length = self.d.textlength(accent, font = fnt)
         char_length = self.d.textlength(char, font = fnt)
         start_diff = math.ceil((char_length - tittle_length) / 2 )
