7e0aa6de137797ce785dbd96617695a90d83b2fe
isör <ratkovszki.istvn@gmail.com>
2022-04-18 23:18:28 +0200

    Updates image generation to have a percentage constant for head border

--- seating_cards/background_drawer.py
+++ seating_cards/background_drawer.py
@@ -13,49 +13,49 @@ class BackgroundDrawer:
 
     def print_background(self):
         #background ellipses
-        bg_padding_w = c.padding + 300
-        bg_padding_h = c.padding + 50
         self.pd.ellipse( 
-                bg_padding_w,
-                bg_padding_h,       
-                c.image_w - 2*bg_padding_w,  
-                c.image_h - 2*bg_padding_h, 
+                c.bg_padding_w,
+                c.bg_padding_h,       
+                c.image_w - 2*c.bg_padding_w,  
+                c.image_h - 2*c.bg_padding_h, 
                 (37,67,147))
 
-        one_w = 100
-        one_h = 100
-        one_p = 50 
         self.pd.ellipse( 
-                bg_padding_w + one_w, 
-                bg_padding_h + one_h, 
-                c.image_w - 2*bg_padding_w - one_w - one_p, 
-                c.image_h - 2*bg_padding_h - one_h - one_p,
+                c.bg_padding_w + c.one_padding + c.one_offset, 
+                c.bg_padding_h + c.one_padding + c.one_offset, 
+                c.image_w - 2*c.bg_padding_w - 2*c.one_padding, 
+                c.image_h - 2*c.bg_padding_h - 2*c.one_padding,
                 (62,94,180))
 
-        two_w = 230
-        two_h = 250
-        two_p = 0
         self.pd.ellipse( 
-                bg_padding_w + two_w, 
-                bg_padding_h + two_h,  
-                c.image_w - 2*bg_padding_w - two_w - two_p - one_w,  
-                c.image_h - 2*bg_padding_h - two_h - two_p - one_h, 
+                c.bg_padding_w + c.two_padding + c.two_offset, 
+                c.bg_padding_h + c.two_padding + c.two_offset,  
+                c.image_w - 2*c.bg_padding_w - 2*c.two_padding,  
+                c.image_h - 2*c.bg_padding_h - 2*c.two_padding, 
                 (85,119,211))
 
         #head circles
         self.pd.ellipse( c.padding, c.image_h - c.padding - c.head_d,  c.head_d, c.head_d, fill=(0,0,0))
         self.pd.ellipse( c.image_w - c.padding - c.head_d, c.padding,  c.head_d, c.head_d, fill=(0,0,0))
 
+
         i = rand.randint(0,len(c.heads)-1)
         images = c.heads[i]
         #add heads
         dog = Image.open('assets/' + images['dog'] + '.png')
         dog = dog.resize((c.head_d, c.head_d))
-        mask = Image.new("L", dog.size, 0)
-        draw = ImageDraw.Draw(mask)
-        draw.ellipse((10,10, c.head_d-10, c.head_d-10), fill=255)
-        c.img.paste(dog, (c.padding, c.image_h - c.padding - c.head_d), mask)
 
         up = Image.open('assets/' + images['up'] + '.png')
         up = up.resize((c.head_d,c.head_d))
+
+        #mask for creating border effect on the heads, otherwise the images are not round
+        mask = Image.new("L", dog.size, 0)
+        draw = ImageDraw.Draw(mask)
+        draw.ellipse((c.head_d * c.head_border_percent / 2,
+                      c.head_d * c.head_border_percent / 2,
+                      c.head_d * (1-c.head_border_percent / 2),
+                      c.head_d * (1-c.head_border_percent / 2)),
+                      fill=255)
+
+        c.img.paste(dog, (c.padding, c.image_h - c.padding - c.head_d), mask)
         c.img.paste(up, (c.image_w - c.padding - c.head_d, c.padding), mask)
